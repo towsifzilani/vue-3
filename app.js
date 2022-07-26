f4437f07bbecfd3bc0e38494b1f3a8838521a03e
towsifzilani <towsifzilani@gmail.com>
2022-07-27 02:27:34 +0600

    Completed Basics

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = Vue.createApp({
     data() {
         return {
             showAuthor: true,
+            url: "https://www.linkedin.com/feed/",
             authors: [
-                {name: "Abdul Kader Zilani", designation: "Sr. Software Engineer", experience: "4+ years"},
-                {name: "Abdul", designation: "Software Engineer", experience: "5+ years"},
-                {name: "Kader", designation: "Sr. Engineer", experience: "6+ years"},
-                {name: "Zilani", designation: "Engineer", experience: "7+ years"},
+                {name: "Abdul Kader Zilani", designation: "Sr. Software Engineer", experience: "4+ years", isFav:true,img:"assets/images/1.jpg"},
+                {name: "Abdul", designation: "Software Engineer", experience: "5+ years", isFav:false,img:"assets/images/2.jpg"},
+                {name: "Kader", designation: "Sr. Engineer", experience: "6+ years", isFav:true,img:"assets/images/3.jpg"},
+                {name: "Zilani", designation: "Engineer", experience: "7+ years", isFav:false,img:"assets/images/1.jpg"},
             ],
             x: 0,
             y: 0
@@ -28,6 +29,14 @@ const app = Vue.createApp({
             this.x = e.offsetX;
             this.y = e.offsetY;
             console.log(e, e.type)
+        },
+        toggleFav(author) {
+            author.isFav = !author.isFav
+        }
+    },
+    computed: {
+        filterAuthors() {
+            return this.authors.filter((author)=> author.isFav)
         }
     }
 })
